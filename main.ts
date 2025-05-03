edf0e73405588f4e348c48315f19260d239de774
Ruslans Platonovs <rplatonovs@gmail.com>
2025-05-03 13:33:42 +0200

    Adds JIRA domain settings mock screen

--- main.ts
+++ main.ts
@@ -1,7 +1,22 @@
-import { Plugin, MarkdownView, Editor } from "obsidian";
+import { Plugin, MarkdownView, Editor, App, PluginSettingTab, Setting } from "obsidian";
 
-export default class JiraLinksPastePlugin extends Plugin {
+interface JiraLinksShortenerPluginSettings {
+	supportedDomain: string;
+}
+
+const DEFAULT_SETTINGS: JiraLinksShortenerPluginSettings = {
+	supportedDomain: '*.atlassian.net'
+}
+
+export default class JiraLinksShortenerPlugin extends Plugin {
+
+  settings: JiraLinksShortenerPluginSettings;
+
   async onload() {
+    
+    await this.loadSettings();
+		this.addSettingTab(new JiraLinksShortenerPluginSettingsTab(this.app, this));
+
     console.log("📋 JIRA links shortener plugin loaded");
 
     this.registerEvent(
@@ -22,4 +37,39 @@ export default class JiraLinksPastePlugin extends Plugin {
       })
     );
   }
+
+  async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+}
+
+class JiraLinksShortenerPluginSettingsTab extends PluginSettingTab {
+	plugin: JiraLinksShortenerPlugin;
+
+	constructor(app: App, plugin: JiraLinksShortenerPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const {containerEl} = this;
+
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName('Apply for domain')
+			.setDesc('You may want to configure your custom JIRA domain if standard config ' +
+				' doesn\'t work for you. You can aso use * for wildcard style definition.')
+			.addText(text => text
+				.setPlaceholder('Enter your JIRA domain')
+				.setValue(this.plugin.settings.supportedDomain)
+				.onChange(async (value) => {
+					this.plugin.settings.supportedDomain = value;
+					await this.plugin.saveSettings();
+				}));
+	}
 }
