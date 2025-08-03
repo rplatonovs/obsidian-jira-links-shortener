91eb4c091c1799bee101843f4ceadaa0b1bcc46d
Dan Riedl <amkoenigsholz9@gmail.com>
2025-08-03 14:01:22 +0200

    feat: support JIRA links with domain paths (reverse proxy setups)

--- utils.ts
+++ utils.ts
@@ -1,18 +1,18 @@
 export function formatJiraLink(pastedText: string, domain: string): string | null {
-	
-	// Avoid modifying text fragments which start from a JIRA link
+	if (!pastedText || typeof pastedText !== 'string' || pastedText.trim() === '') return null;
 	if (/\s/.test(pastedText)) return null;
-	
-	const escDomain = domain.replace(/\./g, '\\.').replace(/(\*)/g, '.*')	
-	const jiraRegex = new RegExp(`^https?:\/\/${escDomain}\/browse\/([A-Z]+-\\d+)(\\?.*)?`, 'g');
+
+	const escDomain = domain.replace(/\./g, '\\.').replace(/(\*)/g, '.*');
+	const jiraRegex = new RegExp(`^https?:\/\/${escDomain}(\/[\\w\\-\\.]+)*\/browse\/(?<issueId>[A-Z]+-\\d+)(\\?.*)?`, 'g');
 
 	if (!jiraRegex.test(pastedText)) return null;
 
-	return pastedText.replace(jiraRegex, (match, issueId) => {
+	return pastedText.replace(jiraRegex, (match, ...groups) => {
+		const groupsObj = groups[groups.length - 1];
+		const issueId = groupsObj?.issueId;
 		return `[${issueId}](${match})`;
 	});
 }
-
-export function satinizeDomain(domain: string): string {
+export function sanitizeDomain(domain: string): string {
 	return domain.replace(/[^a-zA-Z0-9.*-]/g, '')
 }
